27e5963adc76332e4c7e362709737cb9739aa94b
sofia10 <makharashvilisofachka@gmail.com>
2018-06-25 17:20:57 +0400

    .

--- js/details.js
+++ js/details.js
@@ -80,3 +80,11 @@ $(document).ready(function () {
 
 
 });
+
+
+$('#videoModal').on('hide.bs.modal', function(e) {
+    var $if = $(e.delegateTarget).find('iframe');
+    var src = $if.attr("src");
+    $if.attr("src", '/empty.html');
+    $if.attr("src", src);
+});
