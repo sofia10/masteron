ffc03a925d064ebe1918042984c7501313d333b9
sofia10 <makharashvilisofachka@gmail.com>
2018-09-04 12:00:40 +0400

    added rating

--- js/categories.js
+++ js/categories.js
@@ -49,3 +49,32 @@ var swiper = new Swiper('.banners-slider', {
         prevEl: '.swiper-button-prev',
       },
     });
+
+
+
+// rating
+
+
+$.fn.stars = function() {
+    return this.each(function() {
+        var val = parseFloat($(this).html());
+        var size = Math.max(0, (Math.min(5, val))) * 15.8;
+        var $span = $('<span> </span>').width(size);
+        $(this).empty().append($span);
+    });
+}
+
+$(function() {
+    $('.rating-results-content span.rating-stars').stars();
+});
+
+$('.rating-results-content').click(function(ev){
+    var index = ev.pageX;
+    var offset = $(this).offset().left;
+    var clickX = index - offset;
+    var single = $(this).width() / 5;
+
+    var result = Math.ceil(clickX / single);
+    $(this).find('.rating-stars span').css('width', single*result);
+    console.log(result);
+});
